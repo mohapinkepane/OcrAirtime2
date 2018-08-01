b4f47671bba4bb5f35d88fde2968ea4c50b3edf8
Mohapi Nkepane <mohapi765@gmail.com>
2018-08-01 08:40:28 +0200

    my basic app finished

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, LoadingController } from 'ionic-angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera';
+import * as Tesseract from 'tesseract.js'
+import { NgProgress } from '@ngx-progressbar/core';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  selectedImage: string;
+  imageText: string;
 
+  constructor(public navCtrl: NavController, private camera: Camera, private actionSheetCtrl: ActionSheetController, public progress: NgProgress) {
+  }
+
+  selectSource() {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+
+  recognizeImage() {
+    Tesseract.recognize(this.selectedImage)
+      .progress(message => {
+        if (message.status === 'recognizing text')
+          this.progress.set(message.progress);
+      })
+      .catch(err => console.error(err))
+      .then(result => {
+        this.imageText = result.text;
+      })
+      .finally(resultOrError => {
+        this.progress.complete();
+      });
   }
 
 }
